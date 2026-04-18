6cbf21b9b2e56200ca365ad99f278810b2952ad2
zxc1217 <d_ram@163.com>
2026-04-19 00:28:46 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://zxc1217.github.io',
   base: '/',
+  integrations: [sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'rose-pine-dawn',
